481caeea209584269acb50ba61830c1ca3305bd6
bestware <kshitiz.wagle@yahoo.com>
2023-03-26 18:53:51 +0545

    Formatting

--- Lab1/QN1/src/Rectangle.java
+++ Lab1/QN1/src/Rectangle.java
@@ -1,11 +1,9 @@
 public class Rectangle {
-    private double length;
-    private double breadth;
+    private double length, breadth;
     public Rectangle(double length, double breadth) {
         this.length = length;
         this.breadth = breadth;
     }
-
     public double calculateArea() {
         return length * breadth;
     }
--- Lab1/QN2/src/Main.java
+++ Lab1/QN2/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Area rectangle = new Area(23, 32);//for area of rectangle
         Area triangle = new Area(10, 15, 10);
-        System.out.printf("Rectangle %.2f\n", rectangle.calculateAreaRectangle());
-        System.out.printf("Triangle %.2f\n", triangle.calculateAreaTriangle());
+        System.out.printf("Rectangle %.2f\n", rectangle.areaRectangle());
+        System.out.printf("Triangle %.2f\n", triangle.areaTriangle());
     }
 }
